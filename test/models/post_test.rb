baef507cf89d429101cd3f2a0c5d9d221f4b200e
JuanDavidDV <juandadazav@gmail.com>
2025-05-27 12:02:00 -0400

    developed controller and model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:one)
+  end
+
+  test "should be invalid without a title" do
+    @post.title = ""
+    assert_not @post.valid?
+  end
+
+  test "shoul be invalid without a body" do 
+    @post.body = ""
+    assert_not @post.valid?
+  end
+
 end
